29eff9de6c5d39473dcdfd34f847765a3120fd52
Ujjwal <ujjwalag0@gmail.com>
2023-02-11 23:39:36 +0530

    final commit

--- assignment/ujjwal_assignment/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from.models import student , lecturer , class_room
-# Register your models here.
-
-admin.site.register(student)
-admin.site.register(lecturer)
-admin.site.register(class_room)
